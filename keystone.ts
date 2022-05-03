976cd59e411d8957e0e2a11a121a70f7f95eba7b
Lucas Vilela <fleminglucas@gmail.com>
2022-05-04 08:59:04 +1000

    main

--- keystone.ts
+++ keystone.ts
@@ -1,34 +1,18 @@
-/*
-Welcome to Keystone! This file is what keystone uses to start the app.
-
-It looks at the default export, and expects a Keystone config object.
-
-You can find all the config options in our docs here: https://keystonejs.com/docs/apis/config
-*/
-
 import { config } from '@keystone-6/core';
-
-// Look in the schema file for how we define our lists, and how users interact with them through graphql or the Admin UI
 import { lists } from './schema';
-
-// Keystone auth is configured separately - check out the basic auth setup we are importing from our auth file.
 import { withAuth, session } from './auth';
 
 export default withAuth(
-  // Using the config function helps typescript guide you to the available options.
   config({
-    // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
       provider: 'postgresql',
-      url: process.env.DATABASE_URL || "postgres://postgres:postgres@localhost:5432/keystone",
+      url:
+        process.env.DATABASE_URL ||
+        'postgres://postgres:mysecretpassword@localhost:55000',
+      useMigrations: true,
     },
-    // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
-    images: {
-      upload: 'local',
-      local: {
-        storagePath: 'public/images',
-        baseUrl: '/images',
-      },
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data.isAdmin,
     },
     lists,
     session,
