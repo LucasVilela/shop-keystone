8836a567ccd84571e013efa2a3f4dcaffa66684e
Lucas Vilela <fleminglucas@gmail.com>
2022-05-03 17:26:02 +1000

    initial

--- keystone.ts
+++ keystone.ts
@@ -23,10 +23,6 @@ export default withAuth(
       url: 'file:./keystone.db',
     },
     // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
-    ui: {
-      // For our starter, we check that someone has session data before letting them see the Admin UI.
-      isAccessAllowed: (context) => !!context.session?.data,
-    },
     images: {
       upload: 'local',
       local: {
